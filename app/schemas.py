4d030152b322802eb5595cd0e345a2f29df82983
Dev_Lee <toxic023@naver.com>
2023-12-23 21:08:35 +0900

    jwt success

--- app/schemas.py
+++ app/schemas.py
@@ -10,11 +10,10 @@ class SignUpModel(BaseModel):
     is_staff: Optional[bool]
     is_active: Optional[bool]
 
-    model_config = ConfigDict(
-        from_attributes=True,
-        json_schema_extra={
+    class Config:
+        orm_mode = True
+        schema_extra = {
             'example': {
-                'id': 0,
                 'username': 'devLee',
                 'email': 'devLee@gmail.com',
                 "password": "password",
@@ -22,4 +21,12 @@ class SignUpModel(BaseModel):
                 "is_active": True
             }
         }
-    )
+
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = "3866635b4ab9ea251a6fc765f06123469cd91d5a9156eb66e8c0d305c553b1c4"
+
+
+class LoginModel(BaseModel):
+    username: str
+    password: str
